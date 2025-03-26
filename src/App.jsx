91e9d2486d56eaa3ecc06137754a2f47af6b3a4c
Amjad <akioudamjadfone2@gmail.com>
2025-03-26 23:33:29 +0000

    new version

--- src/App.jsx
+++ src/App.jsx
@@ -8,42 +8,44 @@ import Diffrent from './component/diffrentSection.jsx';
 import Join from './component/join.jsx';
 import Footer from './component/footer.jsx';
 import Products from './component/products.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter , Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        
-        <Routes>
-          <Route path="/products" element={<Products />} />
-          <Route path="/" element={
-            <>
-              <section id="home">
-                <Home />
-              </section>
-              <section id="items">
-                <Items />
-              </section>
-              <section id="fashion">
-                <Fashion />
-              </section>
-              <section id="diffrent">
-                <Diffrent />
-              </section>
-              <section id="join">
-                <Join />
-              </section>
-            </>
-          } />
-        </Routes>
-        
-        <section id="footer">
-          <Footer />
-        </section>
-      </div>
-    </Router>
+    <HashRouter>
+      <Router>
+        <div className="container">
+          <Navbar />
+          
+          <Routes>
+            <Route path="/products" element={<Products />} />
+            <Route path="/" element={
+              <>
+                <section id="home">
+                  <Home />
+                </section>
+                <section id="items">
+                  <Items />
+                </section>
+                <section id="fashion">
+                  <Fashion />
+                </section>
+                <section id="diffrent">
+                  <Diffrent />
+                </section>
+                <section id="join">
+                  <Join />
+                </section>
+              </>
+            } />
+          </Routes>
+          
+          <section id="footer">
+            <Footer />
+          </section>
+        </div>
+      </Router>
+    </HashRouter>
   );
 }
 
